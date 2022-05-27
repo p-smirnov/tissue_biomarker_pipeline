8de24746fae582ab7b1a4421d0d84cdfeb2797bf
Petr Smirnov <psmirnov2000@gmail.com>
2022-05-27 10:39:26 -0400

    verify which meta failed in progress

--- verifyWhichMetaFailed.R
+++ verifyWhichMetaFailed.R
@@ -15,7 +15,15 @@ badchars <- "[,]|[;]|[:]|[-]|[+]|[*]|[%]|[$]|[#]|[{]|[}]|[[]|[]]|[|]|[\\^]|[/]|[
 make.names.2 <- function(x) return(gsub(pat=badchars, rep=".", x))
 
 
+test <- fread("/scratch/b/bhaibeka/psmirnov/Tissue_Biomarker/rna/runlist_files/metaHetTestRes.txt")
 
+myfilepath <- "/scratch/b/bhaibeka/psmirnov/Tissue_Biomarker/rna/perm_meta_perm_sig"
 
+##  checking fixed effect res
 test[hetTestRes==0, file.exists(file.path(myfilepath, paste0("permSig_", Gene, "_",make.names.2(Drug), "_", make.names.2(Tissue), "_out.rds")))]
 
+
+## checking julia data output
+myfilepath <- "/scratch/b/bhaibeka/psmirnov/rna/data4juliaBoot"
+
+which(!test[hetTestRes==1, file.exists(file.path(myfilepath, paste0("modelData_", Gene, "_",make.names.2(Drug), "_", make.names.2(Tissue), ".csv")))])
