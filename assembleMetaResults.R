2959a76ce8ccbd189439f4cfce03d1e705f7deeb
Petr Smirnov <psmirnov2000@gmail.com>
2022-10-28 10:59:14 -0400

    prism read in change

--- assembleMetaResults.R
+++ assembleMetaResults.R
@@ -10,15 +10,24 @@ scratch <- Sys.getenv("SCRATCH")
 myDataDir <- file.path(home, "Data", "smallPSets")
 mySigDir <- file.path(scratch, paste0("pearson_",method,"_res"))
 myPvalDir <- file.path(scratch, paste0(method, "_meta_perm_sig"))
-myPvalDir <- file.path(".", paste0(method, "_meta_perm_sig"))
+myPvalDir <- file.path("/zfs-data/tbp_prism_res", paste0(method, "_meta_perm_sig"))
+badchars <- "[,]|[;]|[:]|[-]|[+]|[*]|[%]|[$]|[#]|[{]|[}]|[[]|[]]|[|]|[\\^]|[/]|[\\]|[ ]|[(]|[)]"
 
+make.names.2 <- function(x) {
+    return(gsub(pat = badchars, rep = ".", x))
+}
+
+torunall <- fread("/zfs-data/tbp_prism_res/runlist_files/geneExpressionMasterToRunList.txt",header=F)
+
+corP <- length(unique(torunall[,V1]))
 
 ## First we load in the fixed effect permutation test results:
 
+toRunAfterHet <- fread("/zfs-data/tbp_prism_res/runlist_files/metaHetTestRes.txt")
 
-toRunPerm <- fread("toRunFiles/toRunMetaByGenePerm.txt", header=FALSE)
+toRunPerm <- toRunAfterHet[hetTestRes==0]
 
-colnames(toRunPerm) <- c("Drug", 'Tissue', "Gene")
+# colnames(toRunPerm) <- c("Drug", 'Tissue', "Gene")
 
 # toRunPerm[,hetFilePath := file.path(scratch, "perm_meta_hetPermTest", 
 # 		paste0(make.names(Drug), "_", make.names(Tissue), "_", Gene, "_hetPerm_10000_out.rds"))]
@@ -35,7 +44,7 @@ colnames(toRunPerm) <- c("Drug", 'Tissue', "Gene")
 # toRunPerm[,PermTestExists := sapply(PermFilePath, file.exists)]
 # toRunPerm[!sapply(PermTestExists, any)]
 
-toRunPerm[,PermResFile := file.path(myPvalDir, paste0("permSig_", make.names(Drug), "_", make.names(Tissue), "_", Gene,"_out.rds"))]
+toRunPerm[, PermResFile := file.path(myPvalDir, paste0("permSig_", Gene, "_", make.names.2(Drug), "_", make.names.2(Tissue), "_out.rds"))]
 toRunPerm[,all(sapply(PermResFile, file.exists))]
 
 system.time(toRunPerm[,PermRes := sapply(PermResFile, function(x) list(readRDS(x)))])
@@ -43,8 +52,8 @@ system.time(toRunPerm[,PermRes := sapply(PermResFile, function(x) list(readRDS(x
 toRunPerm[,pvalue := sapply(PermRes, function(x) return(x[[1]]))]
 toRunPerm[,estimate := sapply(PermRes, function(x) return(x[[2]]$t0))]
 
-toRunPerm[, lower := sapply(PermRes, function(x) return(x[["ci"]]$bca[4]))]
-toRunPerm[, upper := sapply(PermRes, function(x) return(x[["ci"]]$bca[5]))]
+toRunPerm[, lower := sapply(PermRes, function(x) return(x[["ci"]]$perc[4]))]
+toRunPerm[, upper := sapply(PermRes, function(x) return(x[["ci"]]$perc[5]))]
 
 
 toRunPerm[order(pvalue)]
@@ -54,37 +63,43 @@ toRunPermOut <- toRunPerm
 toRunPermOut <- toRunPermOut[,PermRes:=NULL]
 toRunPermOut <- toRunPermOut[,PermResFile:=NULL]
 
+toRunPermOut <- toRunPermOut[, hetTestRes := NULL]
+toRunPermOut <- toRunPermOut[, hetFilePath := NULL]
 
-write.csv(toRunPermOut, file="rnaResults/biomarker_res/BiomarkerPermRes.csv")
+write.csv(toRunPermOut, file = "/zfs-data/tbp_prism_res/biomarker_res/BiomarkerPermRes.csv")
 
 
 ### Now lets do the bootstrap results
 
 
 
-home <- Sys.getenv("HOME")
-scratch <- Sys.getenv("SCRATCH")
+# home <- Sys.getenv("HOME")
+# scratch <- Sys.getenv("SCRATCH")
 
-myDataDir <- file.path(home, "Data", "smallPSets")
-mySigDir <- file.path(scratch, paste0("pearson_",method,"_res"))
-bootSigDir <- file.path(scratch, "perm_meta_boot_sig")
-bootSigDir <- file.path(".", paste0(method, "_meta_boot_sig"))
-
-
-
-toRunBoot <- fread("toRunFiles/toRunMetaByGeneBoot.txt", header=FALSE)
-
-colnames(toRunBoot) <- c("Drug", 'Tissue', "Gene")
+# myDataDir <- file.path(home, "Data", "smallPSets")
+# mySigDir <- file.path(scratch, paste0("pearson_",method,"_res"))
+# bootSigDir <- file.path(scratch, "perm_meta_boot_sig")
+bootSigDir <- file.path("/zfs-data/tbp_prism_res", paste0(method, "_meta_boot_sig"))
 
 
 
+# toRunBoot <- fread("toRunFiles/toRunMetaByGeneBoot.txt", header=FALSE)
 
-toRunBoot[,BootSigPath := file.path(bootSigDir, paste0("bootSig_", make.names(Drug), "_", make.names(Tissue), "_", Gene, "_out.rds"))]
+# toRunAfterHet <- fread("/zfs-data/tbp_prism_res/runlist_files/metaHetTestRes.txt")
+
+toRunBoot <- toRunAfterHet[hetTestRes == 1]
+
+# colnames(toRunBoot) <- c("Drug", 'Tissue', "Gene")
+
+
+
+
+toRunBoot[, BootSigPath := file.path(bootSigDir, paste0("bootSig_", Gene, "_", make.names.2(Drug), "_", make.names.2(Tissue), "_out.rds"))]
 
 toRunBoot[,BootSigExists := sapply(BootSigPath, file.exists)]
 toRunBoot[BootSigExists == FALSE]
 toRunBoot <- toRunBoot[BootSigExists == TRUE]
-## Job 1318428 is checking what is going on with these
+## some jobs seem to have failed, run them
 
 toRunBoot <- toRunBoot[,BootRes := sapply(BootSigPath, function(x) return(list(readRDS(x))))]
 
@@ -92,8 +107,8 @@ toRunBoot <- toRunBoot[,BootRes := sapply(BootSigPath, function(x) return(list(r
 toRunBoot[,pvalue := sapply(BootRes, function(x) return(x[[1]]))]
 toRunBoot[,estimate := sapply(BootRes, function(x) return(x[[2]]))]
 
-toRunBoot[, lower := sapply(BootRes, function(x) return(x[["ci"]]$bca[4]))]
-toRunBoot[, upper := sapply(BootRes, function(x) return(x[["ci"]]$bca[5]))]
+toRunBoot[, lower := sapply(BootRes, function(x) return(x[["ci"]]$perc[4]))]
+toRunBoot[, upper := sapply(BootRes, function(x) return(x[["ci"]]$perc[5]))]
 
 
 toRunBoot[order(pvalue)]
@@ -103,6 +118,28 @@ toRunBootOut <- toRunBootOut[,BootRes:=NULL]
 toRunBootOut <- toRunBootOut[,BootSigExists:=NULL]
 toRunBootOut <- toRunBootOut[,BootSigPath:=NULL]
 
+toRunBootOut <- toRunBootOut[, hetTestRes := NULL]
+toRunBootOut <- toRunBootOut[, hetFilePath := NULL]
 
-write.csv(toRunBootOut, file="biomarker_res/BiomarkerBootRes.csv")
+write.csv(toRunBootOut, file = "/zfs-data/tbp_prism_res/biomarker_res/BiomarkerBootRes.csv")
+
+all.res <- rbind(toRunBootOut,toRunPermOut)
+
+all.res[, FWER_genes := pmin(pvalue * corP,1)]
+
+
+gene_info <- fread("geneInfo.csv")
+
+gene_info[, V1 := gsub(V1, pat = "\\.[0-9]+$", rep = "")]
+
+gene_symbol <- gene_info[, .(V1, gene_name)]
+
+colnames(gene_symbol)[1] <- "Gene"
+
+all.res <- merge(all.res, gene_symbol, by = "Gene", all.x = TRUE)
+
+fwrite(all.res, file = "rnaResultsPrism/biomarker_res/mrna_gene_drug_tissue_res_pharmacodb.csv")
+
+
+
 
