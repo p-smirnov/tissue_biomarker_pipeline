2e99be7b79d8d3a3149353ff45576dcda5f681f0
Petr Smirnov <petr@petrwork.uhnres.utoronto.ca>
2025-04-15 13:21:19 +0000

    first step optimizied to use less memory

--- makeMasterToRunRNA.R
+++ makeMasterToRunRNA.R
@@ -3,6 +3,7 @@
 
 library(PharmacoGx)
 library(data.table)
+library(doParallel)
 
 home <- Sys.getenv("HOME")
 scratch <- Sys.getenv("SCRATCH")
@@ -107,9 +108,6 @@ makeToRunTable <- function(pset.list) {
 
     pset.genexp.dt <- pset.genexp.dt[!is.na(value), ]
 
-    setkey(pset.genexp.dt, geneid, PSet, cellid)
-
-
 
     sens.num.dt <- rbindlist(lapply(pset.list, function(x) {
         cbind(
@@ -147,16 +145,32 @@ makeToRunTable <- function(pset.list) {
 
     sens.num.dt.filt[,Drug := as.character(Drug)]
     setkey(sens.num.dt.filt, "Drug","tissueid", "PSet")
+
+    pset.genexp.dt <- merge(pset.genexp.dt, tissueCell.dt, by='cellid')
+
+    setkey(pset.genexp.dt, geneid, tissueid, PSet)
+
     
     toCheckTable <- data.table(expand.grid(drugs_in_3, tissues_in_3, genes_in_3, stringsAsFactors = FALSE))
 
     names(toCheckTable) <- c("Drug", "Tissue", "Gene")
     
+
+    genes.table.dt <- as.data.table(genes.table, keep.rownames = "geneid")
+    gene.pset.map <- melt(genes.table.dt,
+        id.vars = "geneid",
+        variable.name = "PSet",
+        value.name = "present",
+        variable.factor = FALSE # Keep PSet as character
+    )[present == TRUE, .(geneid, PSet)] # Keep only TRUE entries
+    setkey(gene.pset.map, geneid) # Key for fast lookup by gene
+    rm(genes.table.dt) # Clean up intermediate table
+
     check.row <- function(cur.drug, cur.tissue, cur.gene) {
         
-        cur.tissue.cellines <- tissueCell.dt[tissueid == cur.tissue, cellid]
+        # cur.tissue.cellines <- tissueCell.dt[tissueid == cur.tissue, cellid]
 
-        cur.sens.tbl <- sens.num.dt.filt[Drug == cur.drug][tissueid == cur.tissue]
+        cur.sens.tbl <- sens.num.dt.filt[.(cur.drug, cur.tissue)]
 
         # first, we figure out which datasets this drug was tested in
         cur.drug.dataset <- cur.sens.tbl[, unique(PSet)]
@@ -165,7 +179,7 @@ makeToRunTable <- function(pset.list) {
         }
     
         # now, we figure out which datasets this gene was tested in
-        cur.gene.dataset <- colnames(genes.table)[genes.table[cur.gene, ]]
+        cur.gene.dataset <- gene.pset.map[.(cur.gene), PSet, nomatch = 0L]
         if(length(cur.gene.dataset)<3) {
             return(character(0))
         }
@@ -174,8 +188,9 @@ makeToRunTable <- function(pset.list) {
         if(length(common.datasets)<3) {
             return(character(0))
         }
+        
+        cur.genexp.dt <- pset.genexp.dt[.(cur.gene, cur.tissue)]
 
-        cur.genexp.dt <- pset.genexp.dt[geneid == cur.gene, ][cellid %in% cur.tissue.cellines]
 
         common.measurements.in.dataset <- sapply(common.datasets, function(ds) {
             # dataset <- pset.list.genexp[[ds]]
@@ -197,268 +212,40 @@ makeToRunTable <- function(pset.list) {
 
         return(passing.datasets)
     }
-## Hours to run: 24.597
-check.row2 <- function(cur.drug, cur.tissue, cur.gene) {
-    cur.tissue.cellines <- tissueCell.dt[tissueid == cur.tissue, cellid]
 
-    cur.sens.tbl <- sens.num.dt.filt[Drug == cur.drug][tissueid == cur.tissue]
+    toCheckTableList <- split(toCheckTable,toCheckTable$Gene)
 
-    # first, we figure out which datasets this drug was tested in
-    cur.drug.dataset <- cur.sens.tbl[, unique(PSet)]
-    if (length(cur.drug.dataset) < 3) {
-        return(character(0))
+    registerDoParallel(6)
+    out <- foreach(toCheckTable = toCheckTableList) %dopar% {
+        toCheckTable[, check.row(Drug, Tissue, Gene), by = .(Drug, Tissue, Gene)]
     }
 
-    # now, we figure out which datasets this gene was tested in
-    cur.gene.dataset <- colnames(genes.table)[genes.table[cur.gene, ]]
-    if (length(cur.gene.dataset) < 3) {
-        return(character(0))
-    }
-    common.datasets <- intersect(cur.drug.dataset, cur.gene.dataset)
+    out.table <- rbindlist(out)
 
-    if (length(common.datasets) < 3) {
-        return(character(0))
-    }
+    # ## this can be further optimized by execution order I think, for example, the large subsets of pset.genexp.dt by 
 
-    cur.genexp.dt <- pset.genexp.dt[geneid == cur.gene, ][cellid %in% cur.tissue.cellines]
 
-    # common.measurements.in.dataset <- sapply(common.datasets, function(ds) {
-    #     # dataset <- pset.list.genexp[[ds]]
-    #     # cur.gene.values <- assay(dataset, 1)[cur.gene, intersect(cur.tissue.cellines, colnames(dataset))]
-    #     # gene.cellines <- names(cur.gene.values)[!is.na(cur.gene.values)]
-    #     gene.cellines <- cur.genexp.dt[PSet == ds, cellid]
-    #     if (length(gene.cellines) < 20) {
-    #         return(FALSE)
-    #     }
-    #     sens.celllines <- cur.sens.tbl[PSet == ds, cellid]
-    #     return(length(intersect(sens.celllines, gene.cellines)) >= 20)
+    # Rprof()
+    # my.x <- toCheckTable[1:5000, check.row(Drug, Tissue, Gene), by = .(Drug, Tissue, Gene)]
+    # Rprof(NULL)
+    # summaryRprof()$by.self
+
+    # system.time(toCheckTable[1:5000, check.row(Drug, Tissue, Gene), by = .(Drug, Tissue, Gene)])
+
+    # p <- profvis({
+    # my.x <- toCheckTable[1:10000, check.row3(Drug, Tissue, Gene), by = .(Drug, Tissue, Gene)]
     # })
-
-    passing.datasets <- merge(cur.genexp.dt[, .(cellid, PSet)], cur.sens.tbl[, .(cellid, PSet)], on = c("PSet", "cellid"))[, .N, PSet][N >= 20, unique(PSet)]
-
-    if (length(passing.datasets) < 3) {
-        return(character(0))
-    }
-
-    return(passing.datasets)
-}
-
-
-    Rprof()
-    my.x <- toCheckTable[1:5000, check.row(Drug, Tissue, Gene), by = .(Drug, Tissue, Gene)]
-    Rprof(NULL)
-    summaryRprof()$by.self
-
-    system.time(toCheckTable[1:5000, check.row(Drug, Tissue, Gene), by = .(Drug, Tissue, Gene)])
-    system.time(toCheckTable[1:5000, check.row2(Drug, Tissue, Gene), by = .(Drug, Tissue, Gene)])
-
-    p3 <- profvis({
-    my.x <- toCheckTable[1:10000, check.row2(Drug, Tissue, Gene), by = .(Drug, Tissue, Gene)]
-    })
-    p3
-
+    # p
+    return(out.table)
 }
 
 # write.csv(tissue.table, file="tissueIntersectTable.csv")
 
 
-library(reshape2)
-library(data.table)
-library(SummarizedExperiment)
-## Final file output here
+all.dt <- makeToRunTable(pset.list)
 
 
-## TODO: run the full damn merge and see if the numbers match
-
-### Approximate design of this code: filter to drug intersection in at least 3 datasets
-### Filter to tissues with >20 cell lines in at least 3 datasets
-### 
-
-
-tissues_in_3 <- rownames(tissue.table)[apply(tissue.table, 1, function(x) sum(x)>=3)]
-drugs_in_3 <- rownames(drug.table)[apply(drug.table, 1, function(x) sum(x)>=3)]
-
-cells_per_dataset <- sapply(pset.list, function(pset) return(cellNames(pset)[cellInfo(pset)$tissueid %in% tissues_in_3]))
-
-sens.num.dt <- rbindlist(lapply(pset.list, function(x) cbind("PSet" = name(x), 
-	reshape2::melt(summarizeSensitivityProfiles(x, "aac_recomputed")))))
-
-
-colnames(sens.num.dt) <- c("PSet", "Drug", "cellid", "value")
-
-sens.num.dt <- sens.num.dt[cellid %in% .unionList(cells_per_dataset)]
-sens.num.dt <- sens.num.dt[Drug %in% drugs_in_3]
-
-sens.num.dt <- sens.num.dt[!is.na(value)]
-
-## Here we filtered to cell lines in tissues that are in 3+ datasets, and drugs tested on these cell. 
-## We keep only the cells-drug combinations that occur at least 3 times
-
-
-sens.num.dt.filt <- sens.num.dt
-## TODO:: This is wrong, I don't actually want the cellid to be in more than three datasets, just more than 20 cells in at least 3 datasets
-# sens.num.dt.filt <- merge(sens.num.dt[,.N, .(cellid, Drug)][N>=3,.(cellid, Drug)], sens.num.dt)
-
-## Now lets merge this with tissue information for each cell line
-
-tissueCell.dt <- rbindlist(lapply(pset.list, function(x) return(data.frame(cellNames(x), cellInfo(x)[,"tissueid"]))))
-
-tissueCell.dt <- unique(tissueCell.dt)
-
-colnames(tissueCell.dt) <- c("cellid", "tissueid")
-
-sens.num.dt.filt <- merge(sens.num.dt.filt, tissueCell.dt, by="cellid")
-
-## First, we filter to at least 20 cell lines in the tissue
-sens.num.dt.filt <- merge(sens.num.dt.filt[,.N,.(tissueid, Drug, PSet)][N>=20], sens.num.dt.filt, by=c("tissueid", "Drug", "PSet"))
-sens.num.dt.filt[,N:=NULL]
-
-## now, we filter to at least 4 of cell lines with > 5% response in the tissue. 10% would be what DSS uses,
-## but we are being a bit lenient here, since its very unlikely that artefacts from a single dataset 
-## would survive meta-analysis
-sens.num.dt.filt <- merge(sens.num.dt.filt[,sum(value>5),.(tissueid, Drug, PSet)][V1>=4], sens.num.dt.filt, by=c("tissueid", "Drug", "PSet"))
-sens.num.dt.filt[,V1:=NULL]
-
-# Finally, we can drop the value column. 
-sens.num.dt.filt[,value := NULL]
-
-
-
-## Lets extract the gene expresssion values now.
-pset.list.genexp <- lapply(pset.list, function(pset) summarizeMolecularProfiles(pset, mDataNames(pset)))
-
-
-pset.list.genexp <- lapply(pset.list.genexp, function(SE) {
-	rownames(SE) <- gsub(rep="", x=rownames(SE), pat="\\.[0-9]+$")
-	return(SE)
-})
-names(pset.list.genexp) <- names(pset.list)
-
-
-## We filter right away to protein coding genes here. 
-
-pset.list.genexp.m <- lapply(names(pset.list.genexp), function(x) {
-
-
-	## microarray and rnaseq annotations have different column names
-	gene_type_col <- ifelse("GeneBioType" %in% colnames(rowData(pset.list.genexp[[x]])), "GeneBioType", "gene_type") 
-	## limiting feature space for power
-	ft <- rownames(rowData(pset.list.genexp[[x]]))[rowData(pset.list.genexp[[x]])[[gene_type_col]] %in% "protein_coding"]
-
-
-	return(cbind("PSet" = x,reshape2::melt(as.is =TRUE, SummarizedExperiment::assay(pset.list.genexp[[x]])[ft,])))}
-)
-
-
-pset.genexp.dt <- rbindlist(pset.list.genexp.m)
-
-
-colnames(pset.genexp.dt)[2:3] <- c("geneid", "cellid")
-
-
-
-pset.genexp.dt <- pset.genexp.dt[!is.na(value),]
-
-setkey(pset.genexp.dt, geneid)
-
-## I will now filter out geneids not occuring in at least 3 datasets
-
-
-pset.genexp.dt <- merge(pset.genexp.dt,pset.genexp.dt[,length(unique(PSet)), geneid][V1>=3, .(geneid)])
-
-
-## Now filter to only cell lines in my tissues of interest
-
-pset.genexp.dt <- pset.genexp.dt[cellid %in% unique(sens.num.dt.filt$cellid)]
-
-cellTissue.dt <- unique(sens.num.dt.filt[,.(cellid, tissueid)])
-
-pset.genexp.dt <- merge(pset.genexp.dt, cellTissue.dt, by="cellid")
-
-
-## Now check that per-dataset, we have 20 cell lines per tissue per gene
-
-
-
-pset.genexp.dt <- merge(pset.genexp.dt, pset.genexp.dt[,length(unique(cellid)), .(PSet, tissueid, geneid)][V1 >= 20], by=c("PSet", "tissueid", "geneid"))
-
-
-
-pset.genexp.dt[,V1 := NULL]
-
-setkey(pset.genexp.dt, cellid)
-
-pset.genexp.dt[,value := NULL]
-
-## Need to do this by tissue, as I run out of memory
-
-
-pset.genexp.dt.split <- split(pset.genexp.dt, by="tissueid")
-
-sens.num.dt.filt.split <- split(sens.num.dt.filt, by="tissueid")
-
-all.dt.list <- list()
-
-## For each drug, gene, tissue, I need to check that there are 20 cell lines in at least 3 psets. 
-
-
-
-sens.num.dt.filt[,Drug:=droplevels(Drug)]
-
-sens.num.dt.filt.split <- split(sens.num.dt.filt, by="Drug")
-
-
-for(drug in names(sens.num.dt.filt.split)){
-
-
-
-
-	# sens.num.dt.filt.split[[drug]]
-
-	# pset.genexp.dt.split.psets <- split(pset.genexp.dt.split[[tissue]], by="PSet")
-	# sens.num.dt.filt.split.psets <- split(sens.num.dt.filt.split[[tissue]], by="PSet")
-
-
-
-	# all.dt.tissue <- rbindlist(lapply(names(pset.genexp.dt.split.psets), function(pset){
-	# 	pset.genexp.this <- pset.genexp.dt.split.psets[[pset]]
-	# 	sens.num.this <- sens.num.dt.filt.split.psets[[pset]]
-
-	# 	all.dt.tissue <-merge(pset.genexp.this, sens.num.this, by=c("cellid", "PSet", "tissueid"),allow.cartesian=TRUE)
-
-	# 	all.dt.tissue <- merge(all.dt.tissue, all.dt.tissue[,.N, .(Drug, geneid, tissueid, PSet)][N>=20,], by=c("Drug", "tissueid", "geneid", "PSet"))
-
-
-	# }))
-
-	# rm(pset.genexp.dt.split.psets)
-	# rm(sens.num.dt.filt.split.psets)
-
-
-	all.dt.drug <-merge(pset.genexp.dt, sens.num.dt.filt.split[[drug]], by=c("cellid", "PSet", "tissueid"),allow.cartesian=TRUE)
-
-	all.dt.drug <- merge(all.dt.drug, all.dt.drug[,.N, .(Drug, geneid, tissueid, PSet)][N>=20,], by=c("Drug", "tissueid", "geneid", "PSet"))
-	### now that I am sure there are 20 samples per pset for each drug, gene, tissue triplet, I can drop the cellid column
-
-	all.dt.drug[,cellid:=NULL]
-
-	all.dt.drug[,N:=NULL]
-
-	all.dt.drug <- unique(all.dt.drug)
-
-	all.dt.drug <- merge(all.dt.drug, all.dt.drug[,.N, .(geneid, tissueid, Drug)][N >= 3], by=c("geneid", "tissueid", "Drug"))
-
-	all.dt.list[[drug]] <- all.dt.drug
-	gc()
-	print(drug)
-}
-
-
-all.dt <- rbindlist(all.dt.list)
-
-
-all.dt[,N:=NULL]
-all.dt <- all.dt[order(PSet, tissueid, Drug, geneid)]
+all.dt <- all.dt[order(V1, Tissue, Drug, Gene)]
 
 if(!dir.exists(file.path(project, "runlist_files/"))) dir.create(file.path(project, "runlist_files/"))
 
