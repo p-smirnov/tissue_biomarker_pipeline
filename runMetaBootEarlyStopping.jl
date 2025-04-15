ef8af34c06c3001c8edea6e2cf5afd305c76573c
Petr Smirnov <psmirnov2000@gmail.com>
2025-04-15 17:32:43 +0200

    added very rudementary early stopping to the bootstrapping section

--- runMetaBootEarlyStopping.jl
+++ runMetaBootEarlyStopping.jl
@@ -82,25 +82,44 @@ modelData2 = standardizeByDataset(modelData);
 m0 = fit(LinearMixedModel, @formula(y ~ (x + 0| dataset) + x + 0), modelData2);
 t0 = coef(m0)[1];
 
+numPerLoop = 1000;
+
 t = zeros(R);
 
-
-
-for i = 1:R::Int64
-    resampled = getBootSample(modelData);
-    nDSS = length(unique(resampled[!,:dataset]));
-    if nDSS > one(nDSS)
-        @suppress begin
-            m1 = fit(LinearMixedModel, @formula(y ~ (x + 0| dataset) + x + 0), resampled);
-            t[i] = coef(m1)[1];
+i = 1::Int64;
+while i <= R
+    jj = 1::Int64;
+    while jj < (numPerLoop+1)::Int64
+        resampled = getBootSample(modelData);
+        nDSS = length(unique(resampled[!,:dataset]));
+        if nDSS > one(nDSS)
+            @suppress begin
+                m1 = fit(LinearMixedModel, @formula(y ~ (x + 0| dataset) + x + 0), resampled);
+                t[i] = coef(m1)[1];
+            end
+        else
+            @suppress begin
+                m1 = fit(LinearModel, @formula(y ~ x + 0), resampled);
+                t[i] = coef(m1)[1];
+            end
         end
-    else
-        @suppress begin
-            m1 = fit(LinearModel, @formula(y ~ x + 0), resampled);
-            t[i] = coef(m1)[1];
-        end
+        i = i + 1;
+        jj = jj + 1;
+    end
+    if t0 > 0
+        numOverZero = sum(t[1:(i-1)] .< 0);
+    else 
+        numOverZero = sum(t[1:(i-1)] .> 0);
+    end
+    p_hat = numOverZero / (i-1);
+    margin = 1.96 * sqrt(p_hat * (1 - p_hat) / (i - 1)); ## 95% confidence interval
+    if p_hat - margin > 0.05
+        print("Early stopping at $i out of $R using 95% CI bounding p value larger than 0.05\n")
+        break
     end
 end
+
+t = t[1:(i-1)];
 
 # this takes 8 seconds for 1e4, seems to scale linearly from here. 20x improvement!
 
